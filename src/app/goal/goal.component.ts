df4db53d1c8be0a06c4e397467cf74d3b9cbb809
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2021-10-06 18:44:24 +0300

    added bootstrap and some styling

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -11,27 +11,33 @@ export class GoalComponent implements OnInit {
   goals: IGoals[] = [
     {
       "id": 1,
-      "name":'Watch finding Nemo'
+      "name":'Watch finding Nemo',
+      "description":"Find an online version and watch merlin find his son"
     },
     {
       "id":2,
-      "name":'Buy Cookies'
+      "name":'Buy Cookies',
+      "description":"I have to buy cookies for the parrot"
     },
     {
       "id":3,
-      name:'Get new Phone Case'
+      name:'Get new Phone Case',
+      "description":"Diana has her birthday coming up soon"
     },
     {
       "id":4,
-      name:'Get Dog Food'
+      name:'Get Dog Food',
+      "description":"Pupper likes expensive sancks"
     },
     {
       "id":5,
-      "name":'Solve math homework'
+      "name":'Solve math homework',
+      "description":"Damn Math"
     },
     {
       "id":6,
-      "name":'Plot my world domination plan'
+      "name":'Plot my world domination plan',
+      "description":"Cause I am an evil overlord"
     },
   ];
 
--- src/app/goal/goal.ts
+++ src/app/goal/goal.ts
@@ -1,4 +1,5 @@
  export interface IGoals{
    id: number;
    name: string;
+   description: string;
  }
